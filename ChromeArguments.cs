ef15b3da6c09851f68c86c902bf012f325ad8a14
Simontrf <simon.tirouflet@orange.fr>
2025-08-08 09:17:19 +0200

    Add files via upload

--- ChromeArguments.cs
+++ ChromeArguments.cs
@@ -1,9 +1,14 @@
-﻿namespace XmlToPdfConverter.Core.Engine
+﻿using System.IO;
+
+namespace XmlToPdfConverter.Core.Engine
 {
     public static class ChromeArguments
     {
         public static string[] GetChromeArguments(string pdfPath, string xmlUrl, string profilePath)
         {
+            string LogDirectory = Path.Combine(Path.GetDirectoryName(pdfPath), "logs");
+            Directory.CreateDirectory(LogDirectory);
+
             return new string[]
             {
                 "--headless",
@@ -18,9 +23,21 @@
                 "--allow-running-insecure-content",
                 "--allow-file-access-from-files",
                 "--disable-features=VizDisplayCompositor",
-                "--disable-logging",
-                "--silent",
-                "--log-level=3",
+                //"--disable-logging",
+                //"--silent",
+                //"--log-level=3",
+                "--enable-logging=stderr",
+                "--log-level=0",  // 0 = tout, 1 = info, 2 = warning, 3 = error
+                "--v=1",          // Logs verbeux
+                "--vmodule=*=1",  // Tous les modules en verbeux
+
+                "--enable-crash-reporter",
+                "--crash-dumps-dir=\"logs/crashes\"",
+                "--enable-gpu-memory-buffer-video-frames",
+                "--log-gpu-control-list-decisions",
+
+                $"--log-file=\"{LogDirectory}\\chrome.log\"",
+                $"--user-data-dir=\"{profilePath}\"",
                 "--disable-background-timer-throttling",
                 "--disable-backgrounding-occluded-windows",
                 "--disable-renderer-backgrounding",
